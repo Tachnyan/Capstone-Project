4d9302a3ec1a94a1d9866d92d8940adfc153794e
tachnyan <90418666+Tachnyan@users.noreply.github.com>
2022-02-02 16:43:20 -0600

    setup a basic nav bar and stub homepage

---
+++ SITE/smartgroup/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+export const Nav = () => (
+    <nav className="App-nav">
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+        </ul>
+    </nav>
+)
