245a913b1eb25d09b6d3c67b1389b000b0126061
btmulhair <btmulhair@gmail.com>
2022-02-04 14:42:47 -0600

    Updated the page so that it contained a table instead of paragraphs. Filled table with dummy data

--- SITE/studygroupslist/studygroupslist/src/App.js
+++ SITE/studygroupslist/studygroupslist/src/App.js
@@ -9,9 +9,34 @@ function App() {
 	  <button>Back to Menu</button>
 	  <button>Create Group</button>
 	  <button>Join Group</button>
-      <Header title='CurrentStudy Groups'/>
-	  <p>Course / Location / Studying / # People / Timeframe</p>
-	  <p>This should be a selectable menu of all the available groups</p>
+    <Header title='CurrentStudy Groups'/>
+    <table>
+      <thead>
+        <tr>
+          <th>Course</th>
+          <th>Location</th>
+          <th>Studying</th>
+          <th># People</th>
+          <th>Timeframe</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>CSC 130</th>
+          <th>IESB 216</th>
+          <th>Program 1</th>
+          <th>3</th>
+          <th>2pm-4pm</th>
+        </tr>
+        <tr>
+          <th>CSC 131</th>
+          <th>Zoom</th>
+          <th>HW 2</th>
+          <th>2</th>
+          <th>5pm-6pm</th>
+        </tr>
+      </tbody>
+    </table>
     </div>
   );
 }
