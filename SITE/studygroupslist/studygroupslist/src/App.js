8fb6a7488bca8154d4a45df0850d3d299dc02529
btmulhair <btmulhair@gmail.com>
2022-02-04 15:33:24 -0600

    Made the rows of the table highlightable and added more dummy data to the title

--- SITE/studygroupslist/studygroupslist/src/App.js
+++ SITE/studygroupslist/studygroupslist/src/App.js
@@ -13,15 +13,17 @@ function App() {
     <table>
       <thead>
         <tr>
-          <th>Course</th>
-          <th>Location</th>
-          <th>Studying</th>
-          <th># People</th>
-          <th>Timeframe</th>
+          <th scope="col">#</th>
+          <th scope="col">Course</th>
+          <th scope="col">Location</th>
+          <th scope="col">Studying</th>
+          <th scope="col"># People</th>
+          <th scope="col">Timeframe</th>
         </tr>
       </thead>
       <tbody>
         <tr>
+          <th scope="row">1</th>
           <th>CSC 130</th>
           <th>IESB 216</th>
           <th>Program 1</th>
@@ -29,11 +31,28 @@ function App() {
           <th>2pm-4pm</th>
         </tr>
         <tr>
+          <th scope="row">2</th>
           <th>CSC 131</th>
           <th>Zoom</th>
           <th>HW 2</th>
           <th>2</th>
           <th>5pm-6pm</th>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <th>Math 240</th>
+          <th>IESB 114</th>
+          <th>Test 3</th>
+          <th>5</th>
+          <th>4pm-6pm</th>
+        </tr>
+        <tr>
+          <th scope="row">4</th>
+          <th>PHYS 202</th>
+          <th>Tolliver</th>
+          <th>HW 3</th>
+          <th>1</th>
+          <th>11am-1pm</th>
         </tr>
       </tbody>
     </table>
