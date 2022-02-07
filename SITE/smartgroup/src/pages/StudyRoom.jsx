4f55d2acebc662f6476b54eebd2073208d768396
jameswbrooksiii <jameswbrooksiii@gmail.com>
2022-02-07 13:42:22 -0600

    Adjusted chat room window size

--- SITE/smartgroup/src/pages/StudyRoom.jsx
+++ SITE/smartgroup/src/pages/StudyRoom.jsx
@@ -3,12 +3,14 @@ import { ChatEngine } from 'react-chat-engine';
 
 export function StudyRoom() {
     return(
-        <ChatEngine
-            height="100vh"
-            projectID="b94d2416-0dbc-4022-8006-190eb92f5cdf"
-            userName="ChatTest"
-            userSecret="1234"
-        />
+        <div className="chatroom" style={{width:'100%'}}>
+            <ChatEngine
+                height="100vh"
+                projectID="b94d2416-0dbc-4022-8006-190eb92f5cdf"
+                userName="ChatTest"
+                userSecret="1234"
+            />
+        </div>
     )
 }
 
