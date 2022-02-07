9269604b8e8477c624a75b1fb5e0bca25111b7ab
tachnyan <90418666+Tachnyan@users.noreply.github.com>
2022-02-07 15:07:53 -0600

    Register and Login API tests

--- test.js
+++ test.js
@@ -1,16 +1,63 @@
 import test from "ava";
 import axios from 'axios'
 
-test('a+b', (t) =>{
-    t.true(2+2 == 4)
+test.serial('Test Register', async (t) => {
+    let data = {
+        username:"test@latech.edu",
+        password:"test",
+        first:"first",
+        last:"last",
+        studentID:"123-45-678"
+    }
+    await axios.post('http://localhost:3002/sso/register', data, {timeout: 2000})
+    .then((val) => {
+        if(val.status == 200){
+            t.pass()
+        }
+    }).catch((err) => {
+        t.fail()
+    })
 })
 
-test.serial('test login', async (t) => {
+test.serial('Test Authetication', async (t) => {
     let data = {
         username:"test@latech.edu",
         password:"test"
     }
-    await axios.post('localhost:3002/sso/register', data, {timeout: 2000})
+    await axios.post('http://localhost:3002/sso/login', data, {timeout: 2000})
+    .then((val) => {
+        if(val.status == 200){
+            t.pass()
+        }
+    }).catch((err) => {
+        t.fail()
+    })
+})
+
+test.serial('Test Register Rejection', async (t) => {
+    let data = {
+        username:"test@latech.edu",
+        password:"test",
+        first:"first",
+        last:"last",
+        studentID:"123-45-678"
+    }
+    await axios.post('http://localhost:3002/sso/register', data, {timeout: 2000})
+    .then((val) => {
+        if(val.status == 200){
+            t.fail()
+        }
+    }).catch((err) => {
+        t.pass()
+    })
+})
+
+test.serial('Test Authentication rejection', async (t) => {
+    let data = {
+        username:"test@latech.edu",
+        password:"test"
+    }
+    await axios.post('http://localhost:3002/sso/login', data, {timeout: 2000})
     .then((val) => {
         if(val.status == 200){
             t.pass()
