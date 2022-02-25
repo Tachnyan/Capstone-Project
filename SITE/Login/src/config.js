f43c8cda9df173254db6cf74452e14f48d16cb04
tachnyan <90418666+Tachnyan@users.noreply.github.com>
2022-02-25 12:48:45 -0600

    register and login requests hooked to login page

---
+++ SITE/Login/src/config.js
@@ -0,0 +1,8 @@
+const config = {
+    register:"http://127.0.0.1:3002/sso/register",
+    login:"http://127.0.0.1:3002/sso/login",
+    landing:"http://localhost:3000/home"
+}
+
+
+module.exports = {config}
