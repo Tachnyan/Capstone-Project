5a8a9959a0800ed31adf94b48a36f8f8157a6796
jameswbrooksiii <jameswbrooksiii@gmail.com>
2022-02-25 14:04:44 -0600

    Adds routing functionality to previous buttons

--- SITE/smartgroup/src/components/CancelButton.jsx
+++ SITE/smartgroup/src/components/CancelButton.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import { Link } from 'react-router-dom'
 
 export default function CancelButton({content}){
-    return <StyledButton>{content}</StyledButton>
+    return <Link to="/StudyGroupsList"><StyledButton>{content}</StyledButton></Link>
 }
 
 const StyledButton = styled.button`
