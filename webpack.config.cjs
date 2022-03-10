2f28cbf335b8867d22bdcb44519e53181b44fd6c
tachnyan <90418666+Tachnyan@users.noreply.github.com>
2022-03-09 18:00:52 -0600

    Webpack/babel configuration, auth api now serving bundled site files

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -18,26 +18,15 @@ const config = {
                 test: /\.(c|sa|sc|le)ss$/,
                 exclude: /node_modules/,
                 use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                type: 'asset/resource'
             }
         ]
     },
     resolve: {
         extensions: ['*', '.js', '.jsx']
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            "React": "react"
-        })
-    ],
-};
-
-
-const login = Object.assign({}, config, {
-    name: "login",
-    entry: './SITE/Login/src/index.jsx',
-    output: {
-        path: path.resolve(__dirname, './API/SSO/src/LoginBuild'),
-        filename: '[name].js'
     },
     devtool:'inline-source-map',
     plugins: [
@@ -48,7 +37,26 @@ const login = Object.assign({}, config, {
             "React":"react"
         })
     ]
+};
+
+
+const login = Object.assign({}, config, {
+    name: "login",
+    entry: './SITE/Login/src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, './API/SSO/src/LoginBuild'),
+        filename: 'login-[name].js'
+    }
 });
 
+const mainSite = Object.assign({}, config, {
+    name: "mainSite",
+    entry: './SITE/smartgroup/src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, './API/SSO/src/SiteBuild'),
+        filename: 'sb-[name].js'
+    }
+})
 
-module.exports = [login]
+
+module.exports = [login, mainSite]
