78db0fc40359f656cb1790d2cca44e604cf4df1d
Tachnyan <90418666+Tachnyan@users.noreply.github.com>
2022-04-03 11:32:53 -0500

    env config initial setup

--- API/Main/src/db/index.js
+++ API/Main/src/db/index.js
@@ -1,19 +1,13 @@
 import mysql from 'mysql';
+import { pool } from '../config.js'
 
-const pool = mysql.createPool({
-    connectionLimit: 1000,
-    password: 'root', 
-    user: 'root', 
-    database: 'studygroupdb', 
-    host: 'localhost', 
-    port: '3306'
-});
+
 
 function friends(){
 
     return new Promise((resolve, reject) => {
       
-        pool.query('SELECT Student_First, Student_Last FROM student WHERE Student_ID IN (SELECT Student_Friended_ID FROM student_has_friend WHERE Student_User_ID = \'9a8349b3-abf6-11ec-90c1-7c10c952a9ce\')', (err, results) => {
+        pool.query('SELECT Student_First, Student_Last FROM Student WHERE Student_ID IN (SELECT Student_Friended_ID FROM Student_Has_Friend WHERE Student_User_ID = \'9a8349b3-abf6-11ec-90c1-7c10c952a9ce\')', (err, results) => {
 
             if(err){
                 return reject(err);
@@ -27,7 +21,7 @@ function friends(){
 function classmates(){
 
     return new Promise((resolve, reject) => {
-        pool.query('SELECT student_first, student_last FROM student WHERE Student_ID IN (SELECT student_student_id from student_has_course WHERE Course_Course_ID IN (SELECT Course_Course_ID FROM student_has_course WHERE Student_Student_ID = \'9a8349b3-abf6-11ec-90c1-7c10c952a9ce\'))', (err, results) => {
+        pool.query('SELECT Student_First, Student_Last FROM Student WHERE Student_ID IN (SELECT Student_Student_ID from Student_Has_Course WHERE Course_Course_ID IN (SELECT Course_Course_ID FROM Student_Has_Course WHERE Student_Student_ID = \'9a8349b3-abf6-11ec-90c1-7c10c952a9ce\'))', (err, results) => {
             if(err){
                 return reject(err);
             }
@@ -40,7 +34,7 @@ function classmates(){
 function profile(){
 
     return new Promise((resolve, reject) => {
-        pool.query('SELECT * FROM student WHERE Student_ID = \'9a8349b3-abf6-11ec-90c1-7c10c952a9ce\'', (err, results) => {
+        pool.query('SELECT * FROM Student WHERE Student_ID = \'9a8349b3-abf6-11ec-90c1-7c10c952a9ce\'', (err, results) => {
             if(err){
                 return reject(err);
             }
@@ -52,7 +46,7 @@ function profile(){
 function studygroups(){
 
     return new Promise((resolve, reject) => {
-        pool.query('SELECT * FROM studygroup', (err, results) => {
+        pool.query('SELECT * FROM Studygroup', (err, results) => {
             if(err){
                 return reject(err);
             }
