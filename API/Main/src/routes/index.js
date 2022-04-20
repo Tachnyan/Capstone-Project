5d2f8a09ae4dcd43d66f4271f1ceddcf707e06b3
btmulhair <50159221+btmulhair@users.noreply.github.com>
2022-04-20 16:44:52 -0500

    Created nonfunctioning ignore route

--- API/Main/src/routes/index.js
+++ API/Main/src/routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {friends, classmates, profile, studygroups, addfriend, ignoreuser, addcourse, studentcourses,setpreferredname, deletecourse, friendrequests} from '../db/index.js'
+import {friends, classmates, profile, studygroups, addfriend, ignoreuser, addcourse, deletecourse, studentcourses,setpreferredname, friendrequests, ignorelist} from '../db/index.js'
 
 var router = express.Router();
 
@@ -48,6 +48,17 @@ router.get('/friendrequests', async(req, res, next) => {
 
     try{
         let results = await friendrequests(req.query);
+        res.json(results);
+    } catch(e){
+        res.sendStatus(500);
+        console.log(e);
+    }
+})
+
+router.get('/ignorelist', async(req, res, next) => {
+
+    try{
+        let results = await ignorelist(req.query);
         res.json(results);
     } catch(e){
         res.sendStatus(500);
