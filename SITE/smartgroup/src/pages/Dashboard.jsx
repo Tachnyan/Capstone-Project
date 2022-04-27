f0a86d54ee7609b3159f1131156d46e59ab2485f
btmulhair <btmulhair@gmail.com>
2022-04-27 14:58:17 -0500

    Removed add friend and ignore user buttons from dashboard and profile page

--- SITE/smartgroup/src/pages/Dashboard.jsx
+++ SITE/smartgroup/src/pages/Dashboard.jsx
@@ -23,7 +23,6 @@ export const Dashboard = (props) => {
                 <List>
                     {FriendsListRows}       
                 </List>
-                <AddFriend/>
                 </ListDiv>
 
                 {/* Classmates List */}
